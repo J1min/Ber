f73a8f3b93ae31439002a6124b9fecfb77c31a72
J1min <jmhong101506@gmail.com>
2022-07-14 00:44:21 +0900

    NEED :: useRef로 document문 전체 수정

--- pages/days.tsx
+++ pages/days.tsx
@@ -6,50 +6,56 @@ import "react-calendar/dist/Calendar.css";
 import moment from "moment";
 const SCHOOLCODE = 7150658;
 const LOCALCODE = "C10";
-const TODAY = dayjs().format("YYYYMM");
-const API = `https://open.neis.go.kr/hub/SchoolSchedule?Type=Json&ATPT_OFCDC_SC_CODE=${LOCALCODE}&SD_SCHUL_CODE=${SCHOOLCODE}&AA_YMD=${TODAY}`;
 
 export default function Days() {
     const [content, setContent] = useState<object[] | string>([]);
     const tempContent: object[] = [];
     const [marks, setMarks] = useState<string[]>([]);
     const tempMarks: string[] = [];
-    useEffect(() => {
-        axios.get(API).then((response: any) => {
-            for (
-                let i = 0;
-                i < response.data.SchoolSchedule[1].row.length;
-                i++
-            ) {
-                if (
-                    response.data.SchoolSchedule[1].row[i].EVENT_NM !==
-                    "토요휴업일"
-                ) {
-                    // setContent((prev) => [
-                    //     ...prev,
-                    //     {
-                    //         date: response.data.SchoolSchedule[1].row[i].AA_YMD,
-                    //         <content:></content:>
-                    //             response.data.SchoolSchedule[1].row[i].EVENT_NM,
-                    //     },
-                    // ]);
-                    tempContent.push({
-                        date: response.data.SchoolSchedule[1].row[i].AA_YMD,
-                        content:
-                            response.data.SchoolSchedule[1].row[i].EVENT_NM,
-                    });
-                    tempMarks.push(
-                        response.data.SchoolSchedule[1].row[i].AA_YMD
-                    );
-                }
-            }
-            setMarks(tempMarks);
-            setContent(tempContent);
-        });
-    }, []);
-    const tempDate = marks;
     const [value, onChange] = useState(new Date());
-
+    const [today, setToday] = useState(dayjs().format("YYYYMM"));
+    useEffect(() => {
+        setToday(value.toISOString().substr(0, 7).replace("-", ""));
+        const API = `https://open.neis.go.kr/hub/SchoolSchedule?Type=Json&ATPT_OFCDC_SC_CODE=${LOCALCODE}&SD_SCHUL_CODE=${SCHOOLCODE}&AA_YMD=${today}
+        `;
+        try {
+            axios.get(API).then((response: any) => {
+                for (
+                    let i = 0;
+                    i < response.data.SchoolSchedule[1]?.row.length;
+                    i++
+                ) {
+                    if (
+                        response.data.SchoolSchedule[1]?.row[i].EVENT_NM !==
+                        "토요휴업일"
+                    ) {
+                        // setContent((prev) => [
+                        //     ...prev,
+                        //     {
+                        //         date: response.data.SchoolSchedule[1].row[i].AA_YMD,
+                        //         <content:></content:>
+                        //             response.data.SchoolSchedule[1].row[i].EVENT_NM,
+                        //     },
+                        // ]);
+                        tempContent.push({
+                            date: response.data?.SchoolSchedule[1].row[i]
+                                .AA_YMD,
+                            content:
+                                response.data?.SchoolSchedule[1].row[i]
+                                    .EVENT_NM,
+                        });
+                        tempMarks.push(
+                            response.data?.SchoolSchedule[1].row[i].AA_YMD
+                        );
+                    }
+                }
+                setMarks(tempMarks);
+                setContent(tempContent);
+            });
+        } catch (e: any) {
+            console.log("안댐");
+        } 
+    }, [value]);
     return (
         <div
             style={{
@@ -64,9 +70,7 @@ export default function Days() {
                 locale="ko"
                 tileClassName={({ date, view }) => {
                     if (
-                        marks.find(
-                            (x) => x === moment(date).format("YYYYMMDD")
-                        )
+                        marks.find((x) => x === moment(date).format("YYYYMMDD"))
                     ) {
                         return "highlight";
                     }
@@ -74,13 +78,16 @@ export default function Days() {
                 }}
             />
             <ul className="days-board">
-                {content.map((i, ix: number) => {
-                    return (
-                        <li key={ix}>
-                            {i.date} {i.content}
-                        </li>
-                    );
-                })}
+                {
+                    //@ts-ignore
+                    content.map((i, ix: number) => {
+                        return (
+                            <li key={ix}>
+                                {i.date} {i.content}
+                            </li>
+                        );
+                    })
+                }
             </ul>
         </div>
     );
